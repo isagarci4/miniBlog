e75de04491d180333f3811c2052597ee65abe50a
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2023-02-02 10:56:32 -0300

    style: adicionando css a nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css"
 
 const Navbar = () => {
-  return <nav>
-    <NavLink to={"/"}> 
+  return <nav className={styles.navbar}>
+    <NavLink to={"/"} className={styles.brand}> 
         Mini <span>Blog</span>
     </NavLink>
-    <ul>
+    <ul className={styles.links_list}>
         <li>
-            <NavLink to={"/"}>
+            <NavLink to={"/"} className={({isActive}) => (isActive ? styles.active : '')}>
                 Home
             </NavLink>
         </li>
         <li>
-            <NavLink to={"/about"}>
+            <NavLink to={"/about"} className={({isActive}) => (isActive ? styles.active : '')}>
                 Sobre
             </NavLink>
         </li>
